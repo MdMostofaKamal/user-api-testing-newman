0ff531ce28f15843901947d6c3fe4a663ffb6fda
MdMostofaKamal <mdmostofansu@gmail.com>
2022-03-22 16:11:17 +0600

    upload

---
+++ report.js
@@ -0,0 +1,15 @@
+const newman = require('newman');
+newman.run({
+collection: require('./Mostofa.postman_collection.json'),
+//environment: require('./collection/customer_api_env.json'), 
+iterationCount: 1,
+reporters: 'htmlextra',
+reporter: {
+htmlextra: {
+export: './Reports/report.html', 
+}
+}
+}, function (err) {
+if (err) { throw err; }
+console.log('collection run complete!');
+});
